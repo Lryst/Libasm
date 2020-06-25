bb5f3b392f34136299080d3e34826b701bf4c3f4
Lryst <lryst@student.42.fr>
2020-06-25 16:47:22 +0200

    test

--- ft_strlen.s
+++ ft_strlen.s
@@ -1,20 +1,11 @@
 section .text
     global _ft_strlen
 _ft_strlen:
-    xor rdx,rdx
-    mov rbp,rsi
-
-loop:
-    mov al, rbx
-    test al,al
-    jz end
-    inc edx
-    inc ebx
-    jmp loop
-
-end:
-    mov eax,4
-    mov ebx,1
-    mov ecx,rsi
-    mov edx,rbx
-    int 80h
+    xor rax,rax
+    jmp compar
+incr:
+    inc rax
+compar:
+    cmp BYTE [rdi + rax],0
+    jnz incr
+    ret
--- main.c
+++ main.c
@@ -6,17 +6,22 @@
 /*   By: lryst <lryst@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 17:32:55 by lryst             #+#    #+#             */
-/*   Updated: 2020/06/23 22:13:59 by lryst            ###   ########.fr       */
+/*   Updated: 2020/06/25 16:30:06 by lryst            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <inttypes.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int64_t ft_strlen(char *str);
 
 int main()
 {
     //char str[] = "Hello word!";
 
-    printf("%d\n", ft_strlen("coucou"));
+    printf("%ld\n", ft_strlen("coucou"));
     
     return(0);
 }
